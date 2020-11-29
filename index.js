7556142ae576c5c52456bb18757cebbdad358f7e
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-11-28 22:03:43 -0500

    commit tree 277a225d3abfc9c8a7faa887ebc0d428bc0a2d4d

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Your code here
+// Your code here  
+
+sourceArray = [1, 2, 3, -9]
+
+function mapToNegativize(sourceArray){
+  return sourceArray.map(x => x * -1)
+} 
+
+function mapToNoChange (dune) {
+  return dune.map( x => x)
+} 
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(x => x * 2)
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map(x => x * x) //multiply it by itself
+} 
+
+//reduce method 
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    const reducer = function(accumulator, currentValue) {
+        return accumulator + currentValue
+    } 
+    return sourceArray.reduce(reducer, startingPoint)
+} 
+
+function reduceToAllTrue(sourceArray){
+
+    const reducer = function(accumulator, currentValue){  
+        if(!!accumulator == true && !!currentValue == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return sourceArray.reduce(reducer, true)
+}
+
+   
+function reduceToAnyTrue(sourceArray){
+    const reducer = function(accumulator, currentValue){  
+        if(accumulator == true){
+            return true
+        } else {
+            return !!currentValue
+        } 
+    }
+    return sourceArray.reduce(reducer, false) 
+}
